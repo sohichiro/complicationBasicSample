247480c3a5ff996033b4eedcda3be50facb5f209
sohichiro <urochiiuso_ogana_@i.softbank.jp>
2015-07-07 14:46:22 +0900

    Initial Commit

---
+++ complicationBasicSample WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  complicationBasicSample WatchKit Extension
+//
+//  Created by 長尾 聡一郎 on 2015/07/07.
+//  Copyright © 2015年 長尾 聡一郎. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
